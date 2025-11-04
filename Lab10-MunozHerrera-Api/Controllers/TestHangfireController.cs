dcb55d1d4704bcb585288b75a2137273f2c4b45b
Perrito-serio <gean.munoz.herrera@gmail.com>
2025-11-04 14:46:32 -0500

    feat: Añadir endpoint delayed en TestHangfireController para jobs programados

--- Lab10-MunozHerrera-Api/Controllers/TestHangfireController.cs
+++ Lab10-MunozHerrera-Api/Controllers/TestHangfireController.cs
@@ -20,11 +20,22 @@ public class TestHangfireController : ControllerBase
     [HttpPost("fire-and-forget")]
     public IActionResult FireAndForgetJob()
     {
-        
         // Esta es la forma correcta de encolar un job usando Inyección de Dependencias
         BackgroundJob.Enqueue<INotificationService>(
             service => service.SendNotificationAsync("usuario1"));
             
         return Ok("Job 'fire-and-forget' encolado. Revisa tu dashboard de Hangfire.");
     }
+    
+    [HttpPost("delayed")]
+    public IActionResult DelayedJob()
+    {
+        // Usamos .Schedule y le pasamos un TimeSpan de 10 minutos
+        BackgroundJob.Schedule<INotificationService>(
+            service => service.SendNotificationAsync("usuario2"),
+            TimeSpan.FromMinutes(10)); // <-- La diferencia clave
+            
+        return Ok("Job 'delayed' programado para dentro de 10 minutos.");
+    }
+    
 }
