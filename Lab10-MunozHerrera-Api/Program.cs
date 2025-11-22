ead41afe34908673e5b7c0e79634792c01783357
Perrito-serio <gean.munoz.herrera@gmail.com>
2025-11-22 17:33:51 -0500

    fix: solucion de error

--- Lab10-MunozHerrera-Api/Program.cs
+++ Lab10-MunozHerrera-Api/Program.cs
@@ -20,7 +20,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated();
+    context.Database.Migrate();
+    //context.Database.EnsureCreated();
 }
 
 
